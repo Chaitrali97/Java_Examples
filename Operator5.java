1982e5c81ce7f0f845e39ddb01adfd7d2cf6cd42
Chaitrali Jadhav <chaitralee71@gmail.com>
2019-04-18 18:42:12 +0530

    new file added

---
+++ Operator5.java
@@ -0,0 +1,8 @@
+class Operator5{  
+public static void main(String args[]){  
+int a=11;  
+int b=6;  
+int c=21;  
+System.out.println(a<b&&a<c); 
+System.out.println(a<b&a<c);
+}}  
---
+++ Operator6.java
@@ -0,0 +1,9 @@
+class Operator6{  
+public static void main(String args[]){  
+int a=5;  
+int b=20;  
+a+=4;  
+b-=4;
+System.out.println(a);  
+System.out.println(b);  
+}}  
